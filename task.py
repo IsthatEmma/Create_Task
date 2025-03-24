5ecabaf0642d36c33e69851627d030396b9d59ed
IsthatEmma Bautista <emmabautista.2008@gmail.com>
2025-03-24 18:16:18 +0000

    finishing code

--- task.py
+++ task.py
@@ -6,11 +6,7 @@ print("Hello! Welcome to the quiz! Let's do this!!")
 
 def generate_quiz():
     quiz_questions = [] 
-    for i in range(18):
+    for i in range(1):
         quiz_questions.append("What is 2+2?")
-        quiz_questions.append("What is 4+4?")
-    for i in range(18):
-        quiz_questions.append("What is 6+6?")
-    return quiz_questions
+return quiz_questions
 
-print(generate_quiz())
