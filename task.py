0f43b986f4ea2f7fe961c3d5f3c09593f957abe2
IsthatEmma Bautista <emmabautista.2008@gmail.com>
2025-03-26 16:47:07 +0000

    finishing task

--- task.py
+++ task.py
@@ -1,23 +1,38 @@
 import random
 import time
 
+score = 0
+
 quiz_questions = []
 print("Hello! Welcome to the quiz! Let's do this!!")
 
 def generate_quiz():
-    question = "What is 2+2?"
-    print(question)
-    answer1 = input("Enter your answer: ")
+    quiz_question = ("First question! What is 2+2?")
+    print(quiz_question)
+    user_answer = input("Enter your answer: ")
 
-    if answer1 == "4":
+    if user_answer == "4":
         print("Hooray! Thats correct! :D")
     else:
         print("No sorry!:( Thats incorrect")
 
-    return question 
+    return quiz_question 
 
 
 quiz_questions = generate_quiz()
 
 
+def generate_quiz():
+    quiz_question = ("Second question! What is 4+4?")
+    print(quiz_question)
+    user_answer = input("Enter your answer: ")
+
+    if user_answer == "8":
+        print("Hooray! Thats correct! :D")
+    else:
+        print("No sorry!:( Thats incorrect")
+
+    return quiz_question 
+
+quiz_questions = generate_quiz()
 
