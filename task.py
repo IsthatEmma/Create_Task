864d2a891a54c9c3b5d4e42dce2e900ca608e7ff
IsthatEmma Bautista <emmabautista.2008@gmail.com>
2025-04-02 16:18:58 +0000

    finished i think

--- task.py
+++ task.py
@@ -12,13 +12,14 @@ def generate_quiz():
     user_answer = input("Enter your answer: ")
 
     if user_answer == "4":
+        score +=1
         print("Hooray! Thats correct! :D")
     else:
         print("No sorry!:( Thats incorrect")
 
     return quiz_question 
 
-quiz_questions = generate_quiz()
+quiz_questions = create_quiz()
 
 
 def generate_quiz():
@@ -27,6 +28,7 @@ def generate_quiz():
     user_answer = input("Enter your answer: ")
 
     if user_answer == "8":
+        score +=1
         print("Amazing!! Correct again! :D")
     else:
         print("No sorry!:( Thats incorrect")
@@ -34,20 +36,22 @@ def generate_quiz():
     return quiz_question 
 
 
-quiz_questions = generate_quiz()
+quiz_questions = create_quiz()
 
 def generate_quiz():
-    quiz_question = ("Last question! What is 7 x 2")
+    quiz_question = ("Second question! What is 7 x 2?")
     print(quiz_question)
     user_answer = input("Enter your answer: ")
 
     if user_answer == "14":
-        print("Brilliant!:D")
+        score +=1
+        print("Fantastic!! :D")
     else:
-        print("Incorrect, So Sorry!:(")
+        print("No sorry!:(")
 
-    return quiz_question 
+print("You got " + str(score) + "/3")
+return quiz_question 
 
-quiz_questions = generate_quiz()
+quiz_questions = create_quiz()
 
 print("Thanks for playing!")
